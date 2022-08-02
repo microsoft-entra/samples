bfb12ae21f77bca72704de9ca07f9c9be0c999f6
Yoel Hor <yoelhor@live.com>
2022-08-02 10:17:08 +0300

    pasrd

--- fundamentals/user/add/add-user.ps1
+++ fundamentals/user/add/add-user.ps1
@@ -2,7 +2,7 @@ Connect-AzAccount
 
 # Create a password
 $PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
-$PasswordProfile.Password = "PassWord!"
+$PasswordProfile.Password = "<Password>"
 
 # Add a new user with the password profile
 New-AzureADUser -AccountEnabled $True -DisplayName "Abby Brown" -MailNickName "AbbyB" -UserPrincipalName "AbbyB@contoso.com"
--- fundamentals/user/add/readme.md
+++ fundamentals/user/add/readme.md
@@ -5,3 +5,4 @@ This example shows how you can create a new user in Azure AD. In this example we
 For more information, check out the following articles and samples
 
 - [PowerShell](https://docs.microsoft.com/powershell/azure/active-directory/new-user-sample)
+- [Azure CLI](https://docs.microsoft.com/cli/azure/ad/user?view=azure-cli-latest)
